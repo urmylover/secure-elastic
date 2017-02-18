c0889a7bad3df53e08ecfcdb543144382ce29f12
NewFuture <github@newfuture.cc>
2017-02-18 17:11:46 +0800

    add user shell

---
+++ guard/src/user.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+USER=$1
+# HASH=$(docker run -it --rm newfuture/secure-elastic /usr/share/elasticsearch/plugins/search-guard-5/tools/hash.sh $2)
+HASH=$(bash /usr/share/elasticsearch/plugins/search-guard-5/tools/hash.sh -p $2)
+ROLES=${3:-admin}
+
+FILE=
+CONFIG="$USER:\n  hash: '$HASH' #$USER\n  roles:\n   - $ROLES"
+LINE=$(grep -n "^$USER:$" txt | awk -F: '{print $1}')
+if [ $LINE ];then
+    echo "modify user $USER [$ROLES]"
+    sed -i "/^$USER:/,+3d" $FILE
+    sed -i "$LINE i $CONFIG" $FILE
+else
+    echo "add user $USER [$ROLES]"
+    echo "\n$CONFIG\n" >> $FILE
+fi
+
